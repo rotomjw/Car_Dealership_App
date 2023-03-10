7213cc6254fea3f3efb82e33b8eb7b721ac37985
rotomjw <jack@clickandhope.com>
2023-03-10 04:41:24 +0000

    Add project files.

---
+++ Car_Dealership/wwwroot/js/script.js
@@ -0,0 +1,8 @@
+﻿function initMap() {
+    var latlng = new google.maps.LatLng(52.82880107365463, -1.1461487195561435);
+    var options = {
+        zoom: 14, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}
